fe008a5a8eff314da3e5132f9996903849741574
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2023-10-16 12:22:42 +0200

    he añadido la actividad 3

---
+++ Actividades/Actividad3.java
@@ -0,0 +1,16 @@
+package Actividades;
+import java.util.Scanner;
+
+public class Actividad3 {
+    public static void main (String [] args){
+
+        System.out.print("Introduzca un número, por favor: ");
+        Scanner sc = new Scanner(System.in);
+        int num;
+        num = sc.nextInt();
+        if (num)
+
+
+    }
+    
+}
