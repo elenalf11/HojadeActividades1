4026527843d7c92669b61c148849bdd7bad2f554
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2023-10-16 12:09:29 +0200

    actividad 1 hecha

--- Actividades/Actividad1.java
+++ Actividades/Actividad1.java
@@ -1,7 +1,23 @@
 package Actividades;
 
+import java.util.Scanner;
+
 public class Actividad1 {
     public static void main (String [] args){
-        
-    }
+
+		System.out.println("Ingrese su calificación, por favor: ");
+		Scanner sc = new Scanner(System.in);
+		int nota;
+		nota=sc.nextInt();
+		if (nota>=60){
+			System.out.print("Usted está aprobado, enhorabuena ");
+		
+		}
+		else {System.out.print("Usted está suspenso, lo siento");}
+
+	}
+
 }
+
+    
+
