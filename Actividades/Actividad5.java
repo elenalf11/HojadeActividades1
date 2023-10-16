c1b10a1f5f04d986b55e50b7f0701634866f2640
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2023-10-16 17:22:27 +0200

    he hecho  la actividad 5

--- Actividades/Actividad5.java
+++ Actividades/Actividad5.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 
 public class Actividad5 {
     public static void main (String[]args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce un número del 1 al 100");
+        int num = sc.nextInt();
+
+        if (num >= 1 && num <= 100) {
+            System.out.print("El número está dentro del rango");
+
+        } else {System.out.print("El número no está dentro del rango");}
+
+        sc.close();
+
         
     }
     
