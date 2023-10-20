d0a30ce49e2e51afb257c881e84cc8d31f9f7624
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2023-10-20 12:09:12 +0200

    he hecho la actividad 4 del for

--- Actividadesfor/Actividad4.java
+++ Actividadesfor/Actividad4.java
@@ -5,14 +5,22 @@ public class Actividad4 {
     public static void main (String [] args){
         Scanner sc = new Scanner (System.in);
         int numero = sc.nextInt();
+        int factorial = 1;
+        int incremento = 1;
+        System.out.println("Dime un número, por favor: ");
+        sc.close();
 
 
         if(numero>0){
+            for(; incremento < (numero + 1); incremento++){
+                factorial = factorial * incremento;
+                System.out.println(factorial);
+            } System.out.println("El factorial de su número es: " + factorial);
             
         }
         else{System.out.println("Esto es un número negativo, esto no va a funcionar");}
 
-            sc.close();
+            
     }
     
 }
